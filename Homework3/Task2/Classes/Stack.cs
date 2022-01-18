be81266e44aa7f69b3d185b77fa14f3d214fab18
Vita <vitagriciute@gmail.com>
2022-01-18 22:26:44 +0200

    stack added

--- Homework3/Task2/Classes/Stack.cs
+++ Homework3/Task2/Classes/Stack.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Task2.Interfaces;
 
 namespace Task2.Classes
 {
-    class Stack
+    public class Stack<T> : IStack<T>
     {
+        private const int STACK_SIZE = 5;
+
+        public int Index { get; private set; }
+
+        public T[] stack = new T[STACK_SIZE];
+
+        public bool IsEmpty()
+        {
+            return Index == 0;
+        }
+
+        public T Pop()
+        {
+            return Index == 0 ? default : stack[--Index];
+        }
+
+        public void Push(T number)
+        {
+            if (Index == stack.Length)
+            {
+                // does nothing
+                return;
+            }
+
+            stack[Index++] = number;
+        }
     }
 }
