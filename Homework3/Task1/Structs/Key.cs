57d700e24fd4f472fe21194223b5ca3784a67d41
Vita <vitagriciute@gmail.com>
2022-01-18 22:17:42 +0200

    struct added

--- Homework3/Task1/Structs/Key.cs
+++ Homework3/Task1/Structs/Key.cs
@@ -1,12 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Task1.Enums;
 
 namespace Task1.Structs
 {
-    class Key
+    public struct Key : IComparable<Key>
     {
+        readonly Octave Octave;
+        readonly Note Note;
+        readonly Accidental Accidental;
+
+        public Key(Note note, Accidental accidental, Octave octave)
+        {
+            Octave = octave;
+            Note = note;
+            Accidental = accidental;
+        }
+
+        public override string ToString()
+        {
+            var symbol = Accidental.Sharp == Accidental ? "#" :
+                         Accidental.Flat == Accidental ? "♭" : "";
+
+            return $"{Note}{symbol} ({Octave})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var key = (Key)obj;
+
+            if (Octave != key.Octave)
+            {
+                return false;
+            }
+
+            if (key.Note == Note &&
+                key.Accidental == Accidental
+                )
+            {
+                return true;
+            }
+
+            //deals with regular sharp and flat
+            if ((Note + 1 == key.Note &&
+                Accidental == Accidental.Sharp && key.Accidental == Accidental.Flat) ||
+                (Note - 1 == key.Note &&
+                Accidental == Accidental.Flat && key.Accidental == Accidental.Sharp)
+                )
+            {
+                return true;
+            }
+            //deals with one flat and other one no sign
+            else if ((Note + 1 == key.Note &&
+                Accidental == Accidental.NoSign && key.Accidental == Accidental.Flat) ||
+                (Note - 1 == key.Note &&
+                Accidental == Accidental.Flat && key.Accidental == Accidental.NoSign)
+                )
+            {
+                return true;
+            }
+            //deals with A and G
+            else if ((Note == Note.G &&
+                key.Note == Note.A &&
+                Accidental == Accidental.Sharp && key.Accidental == Accidental.Flat) ||
+                (Note == Note.A &&
+                key.Note == Note.G &&
+                Accidental == Accidental.Flat && key.Accidental == Accidental.Sharp)
+                )
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int CompareTo(Key other)
+        {
+            if (this.Equals(other))
+            {
+                return 0;
+            }
+            return Note.CompareTo(other.Note);
+        }
     }
 }
