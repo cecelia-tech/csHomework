2c9f563e670e8ce2701bff835fa8e3bc96d2c179
Vita <vitagriciute@gmail.com>
2022-01-18 22:31:53 +0200

    extension and program

--- Homework3/Task2/Extencion Methods/Extension.cs
+++ Homework3/Task2/Extencion Methods/Extension.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Task2.Classes;
+using Task2.Interfaces;
 
-namespace Task2.Extencion_Methods
+namespace Task2.ExtencionMethods
 {
-    class Extension
+    public static class Extension
     {
+        public static Stack<T> Reverse<T>(this IStack<T> stack)
+        {
+            Stack<T> newStack = new Stack<T>();
+
+            if (stack is Stack<T> stack2)
+            {
+                int stackLength = stack2.Index;
+
+                for (int i = 0; i < stack2.Index; i++)
+                {
+                    newStack.Push(stack2.stack[--stackLength]);
+                }
+            }
+
+            return newStack;
+        }
     }
 }
--- Homework3/Task2/Program.cs
+++ Homework3/Task2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Task2.Classes;
+using Task2.ExtencionMethods;
 
 namespace Task2
 {
@@ -6,7 +8,40 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> st = new Stack<int>();
+
+            Console.WriteLine("Popping from an empty array");
+            Console.WriteLine(st.Pop());
+
+            Console.WriteLine("IsEmpty() method on an empty stack");
+            Console.WriteLine(st.IsEmpty());
+
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+
+            Console.WriteLine("IsEmpty() method on not an empty stack");
+            Console.WriteLine(st.IsEmpty());
+
+            Stack<int> st2 = st.Reverse();
+
+            Console.WriteLine("Printing reversed stack");
+
+            for (int i = 0; i < st2.Index; i++)
+            {
+                Console.WriteLine(st2.stack[i]);
+            }
+
+            //checking different generic type
+            Stack<string> st3 = new Stack<string>();
+
+            st3.Push("1");
+            st3.Push("2");
+            st3.Push("3");
+            st3.Push("4");
+            st3.Push("5");
+            //trying to push into a full stack
+            st3.Push("6");
         }
     }
 }
