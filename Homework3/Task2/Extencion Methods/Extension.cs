285406d386aa8dfbadcfe2cdea020cf29b9680f9
Vita <vitagriciute@gmail.com>
2022-01-18 22:09:12 +0200

    Initial file structure

---
+++ Homework3/Task2/Extencion Methods/Extension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Extencion_Methods
+{
+    class Extension
+    {
+    }
+}
