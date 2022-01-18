b0e6f9b33e6bdcccc7ea1f2d20e785b201ff1dc8
Vita <vitagriciute@gmail.com>
2022-01-18 22:24:59 +0200

    interface

--- Homework3/Task2/Interfaces/IStack.cs
+++ Homework3/Task2/Interfaces/IStack.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task2.Interfaces
+﻿namespace Task2.Interfaces
 {
-    interface IStack
+    public interface IStack<T>
     {
+        public void Push(T item);
+        public T Pop();
+        public bool IsEmpty();
     }
 }
