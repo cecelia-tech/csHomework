4f4f2bff7f017f52ff724478a5cca1cb8018f362
Vita <vitagriciute@gmail.com>
2022-02-14 14:16:41 +0200

    Homework5

---
+++ Homework5/CompanyVacations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyVacations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
