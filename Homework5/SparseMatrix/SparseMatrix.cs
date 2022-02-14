9ca9019c3c4ef79d10450d35b63124cfa6c62771
Vita <vitagriciute@gmail.com>
2022-02-14 14:38:23 +0200

    sparse Matrix added

--- Homework5/SparseMatrix/SparseMatrix.cs
+++ Homework5/SparseMatrix/SparseMatrix.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace SparseMatrix
 {
-    class SparseMatrix
+    public class SparseMatrix : IEnumerable<int>
     {
+        public int Rows { get; }
+        public int Columns { get; }
+
+        private Dictionary<ElementDirections, int> arrayElements = new Dictionary<ElementDirections, int>();
+
+        public SparseMatrix(int columns, int rows)
+        {
+            if (rows <= 0 || columns <= 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                Rows = rows;
+                Columns = columns;
+            }
+
+        }
+
     }
 }
