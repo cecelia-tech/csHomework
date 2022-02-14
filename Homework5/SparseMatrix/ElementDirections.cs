0d3025d3dbc0c14dad7293291ae2b6995bd93824
Vita <vitagriciute@gmail.com>
2022-02-14 14:35:10 +0200

    elementDirections added

--- Homework5/SparseMatrix/ElementDirections.cs
+++ Homework5/SparseMatrix/ElementDirections.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SparseMatrix
 {
-    class ElementDirections
+    public class ElementDirections
     {
+        public int row;
+        public int column;
+
+        public ElementDirections(int column, int row)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        
     }
 }
