237a96c7b78b6ca040da51cbdc5cbaf672f19e43
Vita <vitagriciute@gmail.com>
2022-02-16 13:25:50 +0200

    employees per month added

--- Homework5/CompanyVacations/CompanyVacationInfo.cs
+++ Homework5/CompanyVacations/CompanyVacationInfo.cs
@@ -49,6 +49,33 @@ namespace CompanyVacations
                                       .ToList();
         }
 
+        public IEnumerable<(int, int)> EmployeesPerMonth()
+        {
+            Dictionary<int, int> a = new Dictionary<int, int>();
+
+            var start = allVacationsRecords.Select(x => x.vacationsStart);
+
+            var end = allVacationsRecords.Where(x => x.vacationsStart.Month != x.vacationsEnd.Month)
+                                         .Select(x => x.vacationsEnd);
+
+            start.Concat(end).GroupBy(x => x.Month)
+                             .Select(x => (x.Key, x.Count())).ToList().
+                             ForEach(x => a.Add(x.Key, x.Item2));
+
+            foreach (var month in Enumerable.Range(1, 12))
+            {
+                if (a.ContainsKey(month))
+                {
+                    continue;
+                }
+                else
+                {
+                    a.Add(month, 0);
+                }
+            }
+            return a.OrderBy(x => x.Key).Select(x => (x.Key, x.Value));
+        }
+
 
     }
 
