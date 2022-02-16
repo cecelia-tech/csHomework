d9c4f7e632a2aa0f12cdd336e9016215bb71e515
Vita <vitagriciute@gmail.com>
2022-02-16 13:22:21 +0200

    addEmployeeVacations added

--- Homework5/CompanyVacations/CompanyVacationInfo.cs
+++ Homework5/CompanyVacations/CompanyVacationInfo.cs
@@ -8,6 +8,21 @@ namespace CompanyVacations
 {
     public class CompanyVacationInfo
     {
+        private List<EmployeeVacations> allVacationsRecords = new List<EmployeeVacations>();
 
+        public void AddEmployeeVacations(EmployeeVacations x)
+        {
+            if (x is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (AlreadyOnVacation(x))
+            {
+                throw new ArgumentException("Employee is already on vacation");
+            }
+
+            allVacationsRecords.Add(x);
+        }
     }
 }
