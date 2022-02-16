3833dd9f907bd332c7bef1b656092ab7c0f8b811
Vita <vitagriciute@gmail.com>
2022-02-16 13:04:34 +0200

    helper added

--- Homework5/BookCatalog/Book.cs
+++ Homework5/BookCatalog/Book.cs
@@ -15,6 +15,28 @@ namespace BookCatalog
 
         public Book(string title, DateTime? publicationDate, string ISBN, params string[] authors)
         {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                Title = title;
+            }
+
+            PublicationDate = publicationDate;
+
+            if (authors.Length == 0)
+            {
+                throw new ArgumentException("At least one or more authors needed.");
+            }
+            else
+            {
+                foreach (var author in authors)
+                {
+                    Authors.Add(author);
+                }
+            }
 
         }
     }
--- Homework5/BookCatalog/Helper.cs
+++ Homework5/BookCatalog/Helper.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace BookCatalog
 {
-    public class Helper
+    public static class Helper
     {
+        private static readonly string patternMatch = @"^\d{3}-\d-\d{2}-\d{6}-\d|\d{13}$";
+        private static readonly string hyphenCheck = "-";
+        private static readonly string replaceTo = string.Empty;
 
+        public static string UnifyISBN(this string ISBN)
+        {
+            if (ISBN is null)
+            {
+                throw new ArgumentNullException("ISBN is null");
+            }
+
+            if (Regex.IsMatch(ISBN, patternMatch))
+            {
+                if (Regex.IsMatch(ISBN, hyphenCheck))
+                {
+                    return Regex.Replace(ISBN, hyphenCheck, replaceTo);
+                }
+            }
+            else
+            {
+                throw new FormatException("ISBN has to be digits in either format: XXXXXXXXXXXXX or XXX-X-XX-XXXXXX-X");
+            }
+
+            return ISBN;
+        }
     }
 }
