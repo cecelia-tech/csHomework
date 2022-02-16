750514e63b6a322155da1ce957c70ec2cdd208b7
Vita <vitagriciute@gmail.com>
2022-02-16 13:20:56 +0200

    employee vacationn added

--- Homework5/CompanyVacations/EmployeeVacations.cs
+++ Homework5/CompanyVacations/EmployeeVacations.cs
@@ -8,6 +8,34 @@ namespace CompanyVacations
 {
     public class EmployeeVacations
     {
+        public string Name { get; set; }
+        public TimeSpan VacationsTaken { get; set; }
+        public DateTime vacationsStart { get; set; }
+        public DateTime vacationsEnd { get; set; }
 
+        public EmployeeVacations(string name, DateTime firstVacationDay, DateTime lastVacationDay)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name entry is not valid");
+            }
+            else
+            {
+                Name = name;
+            }
+
+            if ((firstVacationDay.Year != 2021 && lastVacationDay.Year != 2021) ||
+                firstVacationDay >= lastVacationDay)
+            {
+                throw new ArgumentException("Dates are not correct");
+            }
+            else
+            {
+                vacationsStart = firstVacationDay;
+                vacationsEnd = lastVacationDay;
+
+                VacationsTaken = lastVacationDay.Subtract(firstVacationDay);
+            }
+        }
     }
 }
