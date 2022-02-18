82283078608887f620e5eb13354b0f277794eb81
Vita <vitagriciute@gmail.com>
2022-02-19 00:46:38 +0200

    person class updated

--- Homework6/Task1/Person.cs
+++ Homework6/Task1/Person.cs
@@ -9,13 +9,25 @@ namespace Task1
     [TrackingEntity]
     public class Person
     {
+        public string Address { get; set; }
         [TrackingProperty]
         public string Name { get; set; }
+        [TrackingProperty(PropertyName = "Last name")]
         public string LastNmae { get; set; }
-        public int Age { get; set; }
+        [TrackingProperty(PropertyName = "Age")]
+        public int age;
+
 
         public Person()
         {
+        }
+
+        public Person(string name, string lastNmae, int age, string address)
+        {
+            Name = name;
+            LastNmae = lastNmae;
+            this.age = age;
+            Address = address;
         }
     }
 }
